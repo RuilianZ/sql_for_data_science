2da2adc649eab60547861958af4216533f00be5f
RuilianZ <rz2570@cumc.columbia.edu>
2021-12-25 12:54:27 -0500

    finish assignment

--- w3_assignment.sql
+++ w3_assignment.sql
@@ -23,8 +23,43 @@ group by c.customerid
 select
 a.artistid,
 a.title,
-a.artistid,
 t.name,
 t.trackid
 from albums a
 left join tracks t on t.albumid = a.albumid
+
+/*Retrieve a list with the managers last name, and the last name of the employees who report to him or her.*/
+select
+a.lastname as managerlastname,
+b.lastname as empoyeelastname
+from employees a
+inner join employees b
+on a.employeeid = b.reportsto
+
+/*Find the name and ID of the artists who do not have albums.*/
+select
+a.name,
+a.artistid
+fromartists a
+left join albums al
+on al.artistid = a.artistid
+where al.albumid is null
+
+/*Use a UNION to create a list of all the employee's and customer's first names and last names ordered by the last name in descending order.*/
+select
+lastname
+from employees
+union
+select
+lastname
+from customers
+order by lastname desc
+
+/*See if there are any customers who have a different city listed in their billing city versus their customer city.*/
+select
+c.city,
+i.billingcity
+from customers c
+left join invoices i
+on c.customerid = i.customerid
+where c.city <> i.billingcity
